78dea4c0600988a5e194bb016fc476bfb2cd4646
Iwo <iwo.tyszkowski@gmail.com>
2017-05-15 03:46:44 +0200

    step 3

--- gameInventory.py
+++ gameInventory.py
@@ -1,3 +1,5 @@
+import operator
+
 # This is the file where you must work. Write code in the functions, create new functions,
 # so they work according to the specification
 
@@ -29,7 +31,26 @@ def add_to_inventory(inventory, added_items):
 #   in descending order
 # - "count,asc" means the table is ordered by count in ascending order
 def print_table(inventory, order=None):
-    pass
+    items_number = 0
+    for key in inventory:
+        items_number += inventory[key]
+    max_key_length = max(map(len, inventory))
+    if order == 'count,desc':
+        type_of_sort = True
+    elif order == 'count,asc':
+        type_of_sort = False
+    inventory_sorted = sorted(inventory.items(), key=operator.itemgetter(1), reverse=type_of_sort)
+    # Sorting of dict changing it to list of tuples.
+    print('Inventory:')
+    print('  count    \b', ' '*(max_key_length-9), '\bitem name')
+    print('-' * (11 + max_key_length))
+
+    for i in range(len(inventory_sorted)):
+        print(' ' * (6-len(str(inventory_sorted[i][1]))), inventory_sorted[i][1], ' ',
+              ' ' * (max_key_length - len(inventory_sorted[i][0])), inventory_sorted[i][0])
+
+    print('-' * (11 + max_key_length))
+    print('Total number of items:', items_number)
 
 
 # Imports new inventory items from a file
@@ -51,6 +72,11 @@ def export_inventory(inventory, filename="export_inventory.csv"):
 inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 display_inventory(inventory)
 
+print('---> You killed a dragon! Heres a loot')
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 add_to_inventory(inventory, dragon_loot)
 display_inventory(inventory)
+
+print('---> Now we will see it in a proper way!')
+print_table(inventory, "count,desc")
+print_table(inventory, "count,asc")
